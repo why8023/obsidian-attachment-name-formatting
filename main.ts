44ccbe5760efaebb0d49addffbe744c7b26ec062
whying <291424708@qq.com>
2022-03-22 14:17:18 +0800

    replace " " with "_" in filename

--- main.ts
+++ main.ts
@@ -129,7 +129,7 @@ export default class AttachmentNameFormatting extends Plugin {
 					if (attachmentFile instanceof TFile) {
 						// Create the new full name with path
 						let parent_path = attachmentFile.path.substring(0, attachmentFile.path.length - attachmentFile.name.length);
-						let newName = [file.basename, this.settings[fileType], num].join(" ") + "." + attachmentFile.extension;
+						let newName = [file.basename, this.settings[fileType], num].join("_") + "." + attachmentFile.extension;
 						let fullName = parent_path + newName;
 						// Check wether destination is existed
 						let destinationFile = this.app.vault.getAbstractFileByPath(fullName);
