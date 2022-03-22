ebd5b6252bd17daeb956a14c0c0b9e1b2100104a
whying <291424708@qq.com>
2022-03-22 16:10:43 +0800

    update

--- main.ts
+++ main.ts
@@ -129,7 +129,7 @@ export default class AttachmentNameFormatting extends Plugin {
 					if (attachmentFile instanceof TFile) {
 						// Create the new full name with path
 						let parent_path = attachmentFile.path.substring(0, attachmentFile.path.length - attachmentFile.name.length);
-						let newName = attachmentFile.name.replace(" ", "_");
+						let newName = attachmentFile.name.replace(" ", "_").replace("%20", "_");
 						let fullName = parent_path + newName;
 						// Check wether destination is existed
 						let destinationFile = this.app.vault.getAbstractFileByPath(fullName);
